6e1474b85520fb372370fc9100cb604addaec328
Will Lee-Wagner <wleewagner@assetavenue.com>
2016-03-20 04:25:10 -0700

    remove typings from source control

--- typings/browser.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="browser/ambient/moment-timezone/index.d.ts" />
-/// <reference path="browser/ambient/q/index.d.ts" />
-/// <reference path="browser/definitions/lodash/index.d.ts" />
-/// <reference path="browser/definitions/moment/index.d.ts" />
--- typings/main.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="main/ambient/moment-timezone/index.d.ts" />
-/// <reference path="main/ambient/q/index.d.ts" />
-/// <reference path="main/definitions/lodash/index.d.ts" />
-/// <reference path="main/definitions/moment/index.d.ts" />
