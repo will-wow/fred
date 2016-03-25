f8b134a2051c5d2e068a1537e912954233f361c2
Will Lee-Wagner <wleewagner@assetavenue.com>
2016-03-25 16:16:38 -0700

    add test routes

---
+++ scripts/tests.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Description
+//   Some routes for testing hubut functionality.
+//
+// Author:
+//  Will Lee-Wagner <will@assetavenue.com>
+
+module.exports = (robot) => {
+  robot.respond(/what room is this?/i, (res) => {
+    res.send(res.message.room);
+  });
+
+  robot.respond(/test room chat/i, (res) => {
+    res.send('kk');
+
+    setTimeout(function () {
+      this.robot.messageRoom(res.message.room, 'No hashtag');
+      this.robot.messageRoom('#' + res.message.room, 'hashtag');
+    }, 1000);
+  });
+};
