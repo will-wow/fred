c48238130dcd99e1bc236685bab5afc8ebcae3a0
Jordan Chan <jordan@assetavenue.com>
2016-03-27 15:11:10 -0700

    Added random search functionality for hubot

--- scripts/test.js
+++ scripts/test.js
@@ -2,6 +2,10 @@ var yelp = require("node-yelp");
 
 
 module.exports = (robot) => {
+	
+	function feelingLucky(min, max) {
+		return Math.random() * (max - min) + min;
+	}
 
 	var client = yelp.createClient({
 	  	oauth: {
@@ -9,31 +13,31 @@ module.exports = (robot) => {
 		    "consumer_secret": "UsmGIEpuz7DR5q3-vV4TJDFUonM",
 		    "token": "i9_qThJY8s_5XDu9pcS4fjfTVmHy8zFn",
 		    "token_secret": "Gwzyzj38gbvCF6m0lwA_6VQU0KY"
-		 }
+		},
+
+		httpClient: {
+		    maxSockets: 50
+	  	}
 	})
 
 	robot.respond(/yelp me baby/i, (res) => {
 		var randomSearch = 'food';
-		// var data = JSON.stringify({
-		// 	id: 'drgs123',
-		// 	field: 'xQSKK9H7wWD2kGBojduMeQ'
-		// })
+		var limit = '20';
 
 		client.search({
-		  terms: "Café de la presse",
-		  location: "BELGIUM"
-		}).then(function (data) {
-		
-		  console.log(data.businesses[0].url)
+		  location: "1110 Glendon Ave. Los Angeles, CA",
+		  sort: '2',
+		  offset: '15',
+		  radius_filter: '600',
+		  limit: limit,
+		  terms: randomSearch
+		  
+		}).then( (data) => {	
+			
+		  var number = Math.floor(feelingLucky(0 , limit));
+		  var result = data.businesses[number].url;
+		  console.log(result);
+		  
 		});		
-
-		// robot.http('https://api.yelp.com/v2/search?term=food&location=San+Francisco')
-		// .header('Content-Type', 'application/json')
-		// .post(data)(function (err, res, body) {
-		// 	console.log(body)
-		// })
-		
-
-
 	});
 };
