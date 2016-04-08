013d3740a01e315a269b3b83b660e59d35bad5a6
Will Lee-Wagner <wleewagner@assetavenue.com>
2016-04-08 01:33:56 -0700

    add definitions, spelling, and a catchAll

--- src/misc.ts
+++ src/misc.ts
@@ -12,6 +12,8 @@
 // Author:
 //   Will Lee-Wagner <will@assetavenue.com>
 
+import _ = require('lodash');
+
 export = (robot: hubot.Robot) => {
   robot.respond(/what room is this?/i, (res: hubot.Response) => {
     res.send(res.message.room);
@@ -22,12 +24,21 @@ export = (robot: hubot.Robot) => {
     const room: string = res.match[2];
 
     robot.messageRoom(room, message);
-    res.send('Done.')
+    res.send('Done.');
   });
 
   robot.hear(/I (love|like|liek)(?: you)? fred/i, (res: hubot.Response) => {
     const love: string = res.match[1];
 
     res.reply(`I ${love} you too`);
+  });
+
+  robot.catchAll((res) => {
+    // Only respond to direct messages.
+    if (_.words(res.message.text)[0] !== robot.name) {
+      return;
+    }
+
+    res.send(`Sorry, I don't know what you mean! Try saying "fred help"`);
   });
 };
