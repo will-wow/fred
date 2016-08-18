af926f614d4459999d6f9cc48b7ef5babfa2b08b
Will Lee-Wagner <will@whentheresawill.net>
2016-08-18 00:55:53 -0700

    add dictators

---
+++ src/dictator.ts
@@ -0,0 +1,80 @@
+// Description:
+//   Miscellaneous commands
+//
+// Dependencies:
+//
+// Configuration:
+//
+// Commands:
+//   hubot tell me something - Say a random joke or something else from fortune.
+//   hubot what room is this? - Says what Hubot thinks the current room's name is.
+//   hubot say <message> to <room> - Has the bot say a message in a room.
+//   hubot what's our slogan - Say AssetAvenue's slogan
+//   hubot do you want a loan - Ask for a loan
+//   hubot fortune me - Give a random fortune or quote
+//
+// Author:
+//   Will Lee-Wagner <will@assetavenue.com>
+
+import _ = require('lodash');
+
+import nlc from './lib/nlc/naturalLanguageCommander';
+import Dictators from './lib/Dictators';
+
+export = (robot: hubot.Robot) => {
+  const dictators = new Dictators(robot);
+
+  // Add a new dictator.
+  nlc.registerIntent({
+    intent: 'DICTATOR_REGISTER',
+    utterances: [
+       `Add me as a dictator`,
+       `Make me a dictator`
+    ],
+    callback: (res: hubot.Response) => {
+      if (dictators.add(res.message.user.name)) {
+        res.send(`All hail the new dictator!`);
+      } else {
+        res.send(`Good news, you're already a dictator!`);
+      }
+    }
+  });
+
+  // Remove a dictator.
+  nlc.registerIntent({
+    intent: 'DICTATOR_RESIGN',
+    utterances: [
+       `Remove me as a dictator`,
+       `I resign as dictator`,
+       `Stop making me choose lunch`,
+       `Un-Trump me`,
+       `Untrump me`
+    ],
+    callback: (res: hubot.Response) => {
+      if (dictators.remove(res.message.user.name)) {
+        res.send(`Okay, you're no longer a lunch dictator!`);
+      } else {
+        res.send(`Good news, you already weren't a dictator!`);
+      }
+    }
+  });
+
+  // Choose a dictator.
+  nlc.registerIntent({
+    intent: 'DICTATOR_CHOICE',
+    utterances: [
+      `Trump me`,
+      `Who's picking lunch`,
+      `Whos picking lunch`,
+      `What's for lunch`,
+      `Whats for lunch`,
+      `Choose a dictator`,
+      `dictator me`,
+      `choose the dictator`,
+      `lunch time`
+    ],
+    callback: (res: hubot.Response) => {
+      res.send(`@${dictators.choose()} is the dictator today!`);
+    }
+  })
+};
---
+++ src/lib/Dictators.ts
@@ -0,0 +1,66 @@
+import _ = require('lodash');
+
+/** Class representing lunch dictators. */
+class Dictators {
+  namespace: string = 'dictator';
+  dictators: string[];
+
+  /**
+   * Set a reminder for a room.
+   * @param {Object} robot - The hubot robot reference.
+   * @param {string} namespace - The namespace for the reminders in the brain.
+   */
+  constructor(
+    public robot: hubot.Robot
+  ) {
+    // Get the dictators from storage, once they're loaded.
+    this.robot.brain.on('loaded', this._onBrainLoaded.bind(this));
+    // In case they were already loaded before we got here.
+    this._onBrainLoaded();
+  }
+
+  add(username: string): boolean {
+    if (_.includes(this.dictators, username)) {
+      return false;
+    } else {
+      this.dictators.push(username);
+      return true;
+    }
+  }
+
+  remove(username: string): boolean {
+    if (_.includes(this.dictators, username)) {
+      _.remove(this.dictators, username);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  choose(): (string | boolean) {
+    if (this.dictators.length === 0) {
+      return false;
+    } else {
+      return _.sample(this.dictators);
+    }
+  }
+
+  /**
+   * Runs once the robot.brain has loaded. Puts the dictators on this.
+   */
+  private _onBrainLoaded(): void {
+    if (this.dictators) {
+      // No double setup.
+      return;
+    }
+
+    // Set up the brain for the first time, if nessecary.
+    if (!this.robot.brain.data[this.namespace]) {
+      this.robot.brain.data[this.namespace] = [];
+    }
+
+    this.dictators = this.robot.brain.data[this.namespace];
+  }
+}
+
+export default Dictators;
